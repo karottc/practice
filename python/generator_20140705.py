dee8e630b149b2d6f884c12d53e484402c09ac21
karottc <karottc@gmail.com>
2014-07-10 01:49:41 +0800

    增加几个yield的示例在python/generator_20140705.py 文件中.

--- python/generator_20140705.py
+++ python/generator_20140705.py
@@ -38,4 +38,30 @@ def fun(n):
         yield i **2
 
 print list(fun(5))
+t = fun(3)
+print t.next()
+print t.next()
+print t.next()
+#print t.next()      # 现在这句应该会返回一个StopIterator的异常，表明没有下一个元素了
 print
+
+def fab(max):
+    a,b = 0,1
+    i = 0
+    while i < max:
+        yield a
+        a,b = b, a+b
+        i += 1
+f = fab(5)
+print f.next()  # output: 0
+print f.next()  # output: 1
+print f.next()  # output: 1
+print f.next()  # output: 2
+
+for i in fab(10):
+    print i, ',',
+# output: 0 , 1 , 1 , 2 , 3 , 5 , 8 , 13 , 21 , 34 ,
+print
+print list(fab(10))
+for i in list(fab(10)):
+    print i, ',',

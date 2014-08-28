49540ce5bdbb4f34c85ab7e4ebc7131811c22e6c
DChen <karottc@gmail.com>
2014-08-28 13:56:29 +0800

    implement BST-tree's Delete operate.

--- algorithm/bst_tree.cc
+++ algorithm/bst_tree.cc
@@ -75,3 +75,103 @@ void BST_Insert(bst_tree* root, var value)
         }
     }
 }
+
+/* 二叉搜索树：删除 */
+/* 假定根节点不会被删除
+ * 参数：root-根节点，value-要删除的目标值
+ */
+void BST_Delete(bst_tree* root, var value)
+{
+    bst_tree *node = root;
+    bst_tree *parent;
+    while (node != NULL && node->key != value)
+    {
+        parent = node;
+        if (value < node->key)
+        {
+            node = node->left;
+        }
+        else
+        {
+            node = node->right;
+        }
+    }
+    if (node == NULL)
+    {
+        printf("value is not in the tree\n");
+        return;
+    }
+    if (node != root)
+    {
+        /********************************
+         * 目标节点左子节点为空
+         *       O       O
+         *      /         \
+         *     X           X
+         *    / \         / \
+         *  NULL O      NULL O
+         *      / \         / \
+         *     O   O       O   O
+         * */
+        if (node->left == NULL)
+        {
+            if (value < parent->key)
+            {
+                parent->left = node->right;
+            }
+            else
+            {
+                parent->right = node->right;
+            }
+        }
+        /******************************** 
+         * 目标节点右子节点为空
+         *       O       O      
+         *      /         \
+         *     X           X
+         *    / \         / \
+         *   O   NULL    O  NULL
+         *  / \         / \
+         * O   O       O   O
+         * 
+         ********************************/
+        else if (node->right == NULL)
+        {
+            if (value < parent->key)
+            {
+                parent->left = node->left;
+            }
+            else
+            {
+                parent->right = node->left;
+            }
+        }
+        else
+        {
+            /*********************************
+             * 目标节点的子节点都不为空
+             *                 O
+             *                 |
+             *                 X
+             *                / \
+             *               O   O
+             *              / \
+             *             O   O
+             *                  \
+             *                   T
+             *                  / \
+             *                 O   NULL
+             * 用T的值替换目标X的值 
+             ********************************/
+            bst_tree *node1 = node->left;
+            bst_tree *parent1 = node;
+            while (node1->right != NULL)
+            {
+                parent1 = node1;
+                node1 = node1->right;
+            }
+            parent1->right = node1->left;
+            node->key = node1->key;
+        }
+    }
+}

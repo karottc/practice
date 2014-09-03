0e6b829b52faebd2412697a26e79c907e7ccc168
karottc <karottc@gmail.com>
2014-09-04 00:20:33 +0800

    Implement celebrity algorithm.

--- algorithm/README.md
+++ algorithm/README.md
@@ -6,3 +6,4 @@
 2. bst_tree.c: 二叉搜索树的search,insert,delete的实现;
 3. polynomial.cc: 多项式求值的实现;
 4. one_to_one_mapping.cc: 函数集合一对一映射的实现;
+5. celebrity.cc: 社会名流问题的算法实现;
--- algorithm/celebrity.cc
+++ algorithm/celebrity.cc
@@ -9,6 +9,56 @@
 
 int Celebrity(int **know, int n)
 {
+    int celebrity = -1;
     int i = 0, j = 1, next = 3;
     int candidate = 0;
+    while (next <= n)
+    {
+        // 如果i知道j，则i肯定不是社会名流
+        if (know[i][j])
+        {
+            i = next;
+        }
+        else
+        {
+            j = next;
+        }
+        next++;
+    }
+
+    if (i == n)
+    {
+        candidate = j;
+    }
+    else
+    {
+        candidate = i;
+    }
+
+    int wrong = 0;
+    int k = 0;
+    know[candidate][candidate] = 0;
+    while (!wrong && k <= n-1)
+    {
+        if (know[candidate][k])
+        {
+            wrong = true;
+        }
+        if (!know[k][candidate] && candidate != k)
+        {
+            wrong = true;
+        }
+        k++;
+    }
+
+    if (!wrong)
+    {
+        celebrity = candidate;
+    }
+    else
+    {
+        celebrity = -1;
+    }
+
+    return celebrity;
 }

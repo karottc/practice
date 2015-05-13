fe3bacb893566c00883916c5ba329ba16fcef658
karottc <karottc@gmail.com>
2015-05-13 22:22:09 +0800

    优化解析知乎每日吐槽的字段。

--- python/web_spider/test.py
+++ python/web_spider/test.py
@@ -14,44 +14,67 @@ user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36
 
 headers = {"User-agent": user_agent}
 
-url = "http://news-at.zhihu.com/api/4/story/4683248"
+#url = "http://news-at.zhihu.com/api/4/story/4683248"
 # url = "http://news-at.zhihu.com/api/4/section/2"
+url = "http://news-at.zhihu.com/api/4/story/4728744"
 
 request = urllib2.Request(url, headers=headers)
 response = urllib2.urlopen(request)
 
 content = json.loads(response.read())
+body = content['body'].replace('\r','')
 
-bodylines = content['body'].split('\n')
-print content
+bodylines = body.split('\n')
+
 i = 0
 flagAnswer = 0
 answer = ""
-#print len(bodylines)
-print bodylines
-
 for line in bodylines:
-    if line.find('<h2 class="question-title">',0,-1) != -1:
+    if line.find('<h2 class="question-title">') != -1:
         title = line.replace('<h2 class="question-title">','')
         title = title.replace('</h2>','')
-        #print 'title:%s' % title
         continue
-
+    if line.find('<img class="avatar"') != -1:
+        avatar = line.replace('<img class="avatar" src="','')
+        avatar = avatar.replace('">','')
+        continue
+    if line.find('<span class="author">') != -1:
+        tempList = line.split('span')
+        author = tempList[1].replace(' class="author">','')
+        author = author.replace('，</','')
+        bio = tempList[3].replace(' class="bio">','')
+        bio = bio.replace('</','')
     if line == '<div class="content">':
         flagAnswer = 1
     elif flagAnswer == 1 and line != '</div>':
         #str = "%s\n" % line
         line = line.replace('<p>','')
         line = line.replace('</p>','')
-        line = line.replace('\r','')
-        answer += "%s," % line
+        if line.find('<img class="content-image"') != -1:
+            tempList = line.split('"')
+            line = '<img src="%s" />' % tempList[3]
+        answer += "%s\n" % line
         #print line
     elif flagAnswer == 1 and line == '</div>':
         flagAnswer = 0
+        '''
         print "title:%s" % title
+        print "Autor:%s (%s) - %s" % (author, avatar,bio)
         print "answer:%s" % answer
         print '------------------------------'
         answer = ""
+        '''
+        continue
+    if line.find('<div class="view-more">') != -1:
+        tempList = line.split('"')
+        moreUrl = tempList[3]
+        print "title:%s" % title
+        print "Autor:%s (%s) - %s" % (author, avatar,bio)
+        print "answer:%s" % answer
+        print "view-more: %s" % moreUrl
+        print '------------------------------'
+        answer = ""
+
 
 print "======================================"
 for val in content["recommenders"]:
